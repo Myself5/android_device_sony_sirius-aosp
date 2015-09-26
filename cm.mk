060fbbec1c44ab5307645cdd0d5d777c10a7cc8b
Myself5 <myself5@carbonrom.org>
2015-09-26 19:21:13 -0400

    add _aosp device to asserts

--- cm.mk
+++ cm.mk
@@ -37,3 +37,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_DEVICE := sirius_aosp
 PRODUCT_NAME := cm_sirius_aosp
 PRODUCT_MODEL := Xperia Z2
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := D6502,D6503,D6506,D6543,sirius,sirius_aosp
